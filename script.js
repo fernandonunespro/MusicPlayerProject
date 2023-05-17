212d4c357556ac2422602528329994bc2dbf3f6b
Fernando Nunes de Almeida <nunesfernandopro@gmail.com>
2023-05-17 22:06:47 +0100

    Update

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const next = document.getElementById("next");
 const previous = document.getElementById("previous");
 const currentProgress = document.getElementById("current-progress");
 const progressContainer = document.getElementById("progress-container");
+const shuffleButton = document.getElementById("shuffle");
 
 const boateAzul = {
   songName: "Boate Azul",
@@ -27,7 +28,9 @@ const terminaComigo = {
   file: "termina-comigo-antes",
 };
 let isPlaying = false;
-const playlist = [boateAzul, lifeGoesOn, terminaComigo];
+let isShuffled = false;
+const originalplaylist = [boateAzul, lifeGoesOn, terminaComigo];
+const sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function playSong() {
@@ -53,15 +56,15 @@ function playPauseDecider() {
 }
 
 function initializeSong() {
-  cover.src = `images/${playlist[index].file}.webp`;
-  song.src = `songs/${playlist[index].file}.mp3`;
-  songName.innerText = playlist[index].songName;
-  bandName.innerText = playlist[index].artist;
+  cover.src = `images/${sortedPlaylist[index].file}.webp`;
+  song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
 }
 
 function previousSong() {
   if (index === 0) {
-    index = playlist.length - 1;
+    index = sortedPlaylist.length - 1;
   } else {
     index -= 1;
   }
@@ -70,7 +73,7 @@ function previousSong() {
 }
 
 function nextSong() {
-  if (index === playlist.length - 1) {
+  if (index === sortedPlaylist.length - 1) {
     index = 0;
   } else {
     index += 1;
@@ -91,6 +94,30 @@ function jumpTo(event){
     song.currentTime = jumpToTime;
 }
 
+function shuffleArray(preShuffleArray) {
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while (currentIndex > 0) {
+    let randomIndex = Math.floor(Math.random() * size);
+    let aux = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+    preShuffleArray[randomIndex] = aux;
+    currentIndex -= 1;
+  }
+}
+
+function shuffleButtonClicked() {
+  if (isShuffled === false) {
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add('button-active');
+  } else {
+    isShuffled = false;
+    sortedPlaylist = [...originalPlaylist];
+    shuffleButton.classList.remove('button-active');
+  }
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
@@ -98,3 +125,4 @@ previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
 song.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
